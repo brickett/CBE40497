0da4a17b29629a463450e675eebecaa0339eeda7
brickett <brickett@users.noreply.github.com>
2017-01-27 21:49:24 -0500

    Built input functions

--- SRK-EoS.py
+++ SRK-EoS.py
@@ -5,3 +5,9 @@ Created on Wed Jan 25 08:45:16 2017
 @author: Bryan Ricketts
 """
 
+# The function takes inputs of temperature (K) and pressure (bar)
+temp=input("Temperature in K = ")
+pres=input("Pressure in bar = ")
+# convert to float
+temp=float(temp)
+pres=float(pres)
